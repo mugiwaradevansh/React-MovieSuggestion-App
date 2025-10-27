b402b90c8f069660450842096308a79807989f4a
devansh <dsppatel.149@gmail.com>
2025-10-27 20:31:11 +0530

    deploys

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
 // Entry point: mount the React application into #root
 // StrictMode helps surface potential problems in development (double-invoking certain lifecycle calls)
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
